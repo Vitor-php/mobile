4050d02076b588abf6403d9c2eec70aa5177f4ff
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-09-16 02:28:26 -0300

    fix: second class the NLW finished

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  logoImg: {
+    width: 214,
+    height: 120,
+    top: 74,
+    marginBottom: 110
+  },
+  contentList: {
+    paddingLeft: 32,
+    paddingRight: 64,
+  }
+});
