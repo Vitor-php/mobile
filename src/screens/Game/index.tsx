3b07fdc4dde136174d1a206378ddf079266943d4
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-09-21 15:30:33 -0300

    fix: formatting code

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -4,14 +4,13 @@ import { useRoute, useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { GameParams } from "../../@types/navigation";
 import { styles } from "./styles";
-import { Entypo } from '@expo/vector-icons';
+import { Entypo } from "@expo/vector-icons";
 import { THEME } from "../../theme";
 import logoImg from "../../assets/logo-nlw-esports.png";
 
 import { Background } from "../../components/Background";
 import { Heading } from "../../components/Heading";
 import { DuoCard, DuoCardProps } from "../../components/DuoCard";
-
 
 export function Game() {
   const route = useRoute();
@@ -25,8 +24,8 @@ export function Game() {
 
   useEffect(() => {
     fetch(`http://192.168.0.208:3333/games/${game.id}/ads`)
-      .then(response => response.json())
-      .then(data => setDuos(data))
+      .then((response) => response.json())
+      .then((data) => setDuos(data));
   }, []);
 
   return (
@@ -34,51 +33,41 @@ export function Game() {
       <SafeAreaView style={styles.container}>
         <View style={styles.header}>
           <TouchableOpacity onPress={handleGoBack}>
-            <Entypo 
-            name="chevron-thin-left"
-            color={THEME.COLORS.CAPTION_300}
-            size={20}
+            <Entypo
+              name="chevron-thin-left"
+              color={THEME.COLORS.CAPTION_300}
+              size={20}
             />
           </TouchableOpacity>
 
-          <Image
-            source={logoImg}
-            style={styles.logo}
-          />
+          <Image source={logoImg} style={styles.logo} />
           <View style={styles.right} />
-
         </View>
 
         <Image
-          source={{uri: game.bannerUrl}}
+          source={{ uri: game.bannerUrl }}
           style={styles.cover}
           resizeMode="cover"
         />
 
-          <Heading 
-            title={game.name}
-            subtitle="Conecte-se e comece a jogar!"
-          />
+        <Heading title={game.name} subtitle="Conecte-se e comece a jogar!" />
 
-          <FlatList
-            data={duos}
-            keyExtractor={item => item.id}
-            renderItem={({ item }) => (
-              <DuoCard
-                onConnect={() => {}}
-                data={item}
-              />
-            )}
-            horizontal
-            style={styles.containerList}
-            contentContainerStyle={[duos.length > 0 ? styles.contentList : styles.emptyListContent ]}
-            showsHorizontalScrollIndicator={false}
-            ListEmptyComponent={() => (
-              <Text style={styles.emptyListText}>
-                Nenhum anúncio encontrado
-              </Text>
-            )}
-          />
+        <FlatList
+          data={duos}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <DuoCard onConnect={() => {}} data={item} />
+          )}
+          horizontal
+          style={styles.containerList}
+          contentContainerStyle={[
+            duos.length > 0 ? styles.contentList : styles.emptyListContent,
+          ]}
+          showsHorizontalScrollIndicator={false}
+          ListEmptyComponent={() => (
+            <Text style={styles.emptyListText}>Nenhum anúncio encontrado</Text>
+          )}
+        />
       </SafeAreaView>
     </Background>
   );
